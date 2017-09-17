72c0f84300c63e943aa7b358d573a9e1c0ec5c56
rhiroy <rhiannonleparmentier@Rhiannons-MacBook.local>
2017-09-17 16:45:35 -0500

    Basic server-side rendering of React hello world

---
+++ src/client/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = (props) => (
+  <div>
+	<p>Hello World! zomg React works. okay coolz</p>
+  </div>
+);
+
+export default App;
---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+import ReactDOMServer from 'react-dom/server';
+import React from 'react';
+import App from '../client/App'
+var app = express();
+
+// app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  res.send(ReactDOMServer.renderToString(<App />))
+});
+
+app.listen(3000, function() {
+  console.log('Graffiti is listening on 3000');
+});
